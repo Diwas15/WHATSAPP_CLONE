92f700f02d5d48e2f20ca1b3311ddfe85b07eca5
Diwas Sati <satidiwas@gmail.com>
2025-05-10 14:25:33 +0530

    second change to temp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 // comment added to this
+//changed
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
