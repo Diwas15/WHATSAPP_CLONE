03109170552ffbd9573e96a8ce74307e14c23cd0
Diwas Sati <satidiwas@gmail.com>
2025-05-10 14:28:30 +0530

    develop commited 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 // comment added to this
+//naya hai
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
