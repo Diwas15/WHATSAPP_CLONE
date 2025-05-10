ad9a9073df119ab1d6c81aae7854263228d28c01
Diwas Sati <satidiwas@gmail.com>
2025-05-10 14:40:24 +0530

    this is for developer first

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 // comment added to this
 //changed
+////////this is important cndcjns to develoepr firt
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
