3211022b9c34f02f8962ed8256892f67f0723d41
Diwas Sati <satidiwas@gmail.com>
2025-05-10 15:20:11 +0530

    this is my mesage

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// comment added to this
-//naya hai
-//changed
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
